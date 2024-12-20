623fd7731fa640b68265760a299f3624026d91c7
phanben110 <phanben110@gmail.com>
2024-12-20 19:05:54 +0000

    update run without wandb

--- src/FoodHazardDetectionSemEval2025.py
+++ src/FoodHazardDetectionSemEval2025.py
@@ -25,8 +25,9 @@ warnings.filterwarnings("ignore")
 
 
 class FoodHazardDetectionSemEval2025:
-    def __init__(self, config):
+    def __init__(self, config, use_wandb = True):
         self.config = config
+        self.use_wandb = use_wandb
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Load tokenizer and initialize model paths
@@ -46,11 +47,12 @@ class FoodHazardDetectionSemEval2025:
         # Initialize WandB
         name = config["wandb_run_name"] + "_" + str(config['max_len']) 
         print("name:", name)
-        self.wandb = wandb.init(
-            project=config["wandb_project_name"],
-            name=name,
-            config=config,
-        )
+        if self.use_wandb: 
+            self.wandb = wandb.init(
+                project=config["wandb_project_name"],
+                name=name,
+                config=config,
+            )
 
         # Create logs folder
         os.makedirs("logs", exist_ok=True)
@@ -148,13 +150,14 @@ class FoodHazardDetectionSemEval2025:
                 val_losses.append(val_loss)
 
                 # Log metrics to WandB
-                self.wandb.log({
-                    "Train Loss": train_loss, "Train Precision": train_precision,
-                    "Train Recall": train_recall, "Train F1": train_f1,
-                    "Val Loss": val_loss, "Val Precision": val_precision,
-                    "Val Recall": val_recall, "Val F1": val_f1,
-                    "Learning Rate": self.optimizer.param_groups[0]["lr"],  # Log current LR
-                })
+                if self.use_wandb: 
+                    self.wandb.log({
+                        "Train Loss": train_loss, "Train Precision": train_precision,
+                        "Train Recall": train_recall, "Train F1": train_f1,
+                        "Val Loss": val_loss, "Val Precision": val_precision,
+                        "Val Recall": val_recall, "Val F1": val_f1,
+                        "Learning Rate": self.optimizer.param_groups[0]["lr"],  # Log current LR
+                    })
 
                 print(f"Train -- Loss: {train_loss:.4f} | Precision: {train_precision:.4f} | Recall: {train_recall:.4f} | F1: {train_f1:.4f}")
                 print(f"Val   -- Loss: {val_loss:.4f} | Precision: {val_precision:.4f} | Recall: {val_recall:.4f} | F1: {val_f1:.4f}")
@@ -183,7 +186,8 @@ class FoodHazardDetectionSemEval2025:
         print(f"Best Model: Epoch {best_epoch}, Best Validation Loss: {best_val_loss:.4f}")
         print(f"Path to log file: {self.log_filename}")
         # wandb.finish()
-        self.wandb.finish() 
+        if self.use_wandb:
+            self.wandb.finish() 
 
 
     def load_label_encoder(self, label_encoder_path):
@@ -248,62 +252,6 @@ class FoodHazardDetectionSemEval2025:
 
         return predicted_labels, predicted_probs
 
-    # def train_and_evaluate(self):
-    #     train_losses, val_losses = [], []
-    #     best_val_loss = float("inf")
-    #     best_epoch = 0
-
-    #     with open(self.log_filename, "a") as log_file:
-    #         for epoch in range(self.config["epochs"]):
-    #             start_time = datetime.now()
-
-    #             print(f"Epoch {epoch + 1}/{self.config['epochs']}")
-
-    #             # Training
-    #             train_loss, train_precision, train_recall, train_f1 = train(
-    #                 self.model, self.train_loader, self.optimizer, self.criterion, self.device
-    #             )
-    #             train_losses.append(train_loss)
-
-    #             # Validation
-    #             val_loss, val_precision, val_recall, val_f1 = evaluate(
-    #                 self.model, self.val_loader, self.criterion, self.device
-    #             )
-    #             val_losses.append(val_loss)
-
-    #             # Log metrics to WandB
-    #             self.wandb.log({
-    #                 "Train Loss": train_loss, "Train Precision": train_precision,
-    #                 "Train Recall": train_recall, "Train F1": train_f1,
-    #                 "Val Loss": val_loss, "Val Precision": val_precision,
-    #                 "Val Recall": val_recall, "Val F1": val_f1,
-    #             })
-
-    #             print(f"Train -- Loss: {train_loss:.4f} | Precision: {train_precision:.4f} | Recall: {train_recall:.4f} | F1: {train_f1:.4f}")
-    #             print(f"Val   -- Loss: {val_loss:.4f} | Precision: {val_precision:.4f} | Recall: {val_recall:.4f} | F1: {val_f1:.4f}")
-
-    #             log_file.write(f"Epoch {epoch + 1}/{self.config['epochs']}\n")
-    #             log_file.write(f"Train -- Loss: {train_loss:.4f} | Precision: {train_precision:.4f} | Recall: {train_recall:.4f} | F1: {train_f1:.4f}\n")
-    #             log_file.write(f"Val   -- Loss: {val_loss:.4f} | Precision: {val_precision:.4f} | Recall: {val_recall:.4f} | F1: {val_f1:.4f}\n")
-    #             log_file.flush()
-
-    #             # Save best model
-    #             if val_loss < best_val_loss:
-    #                 best_val_loss = val_loss
-    #                 best_epoch = epoch + 1
-    #                 torch.save(self.model.state_dict(), self.best_model_path)
-
-    #             # Time tracking
-    #             elapsed_time = datetime.now() - start_time
-    #             remaining_time = (self.config["epochs"] - epoch - 1) * elapsed_time
-    #             print(f"Time Elapsed: {elapsed_time}, Time Remaining: {remaining_time} \n")
-
-    #     # Save final model
-    #     torch.save(self.model.state_dict(), self.final_model_path)
-    #     print(f"Best Model: Epoch {best_epoch}, Best Validation Loss: {best_val_loss:.4f}")
-    #     print(f"Path to log file: {self.log_filename}")
-    #     # wandb.finish()
-    #     self.wandb.finish() 
 
 
 if __name__ == "__main__":
